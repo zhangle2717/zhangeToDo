aba727864cc3712826e6b5d59fe5d6450d4ef7ba
zhangle2717 <zhangle2717@163.com>
2018-10-27 09:12:42 +0800

    todo完成

---
+++ js/app.js
@@ -0,0 +1,62 @@
+(function (window, Vue) {
+		// var list = [{ content: 'a', id: 1, isFinish: false },
+		// { content: 'b', id: 2, isFinish: true },
+		// { content: 'c', id: 3, isFinish: false },]
+	new Vue({
+		el: "#app",
+		data: {
+			list: JSON.parse(window.localStorage.getItem('list'))||[],
+			setVal: "",
+		},
+		methods: {
+			addVal() {
+				this.list.push({
+					content: this.setVal,
+					id: this.list.length ? this.list.sort((a,b) => a.id - b.id).slice(this.list.length - 1,)[0].id + 1 : 1,
+					isFinish: false
+				}),
+				this.setVal="";
+			},
+			dele(index){
+				this.list.splice(index,1)
+			},
+			deleAll(){
+			this.list=	this.list.filter((e)=>e.isFinish===false);
+			}
+		},
+		//设置自定指令
+		directives: {
+			foucs: {
+				inserted(el) {
+					el: focus();
+				}
+			}
+		},
+		computed:{
+			numb(){
+			return	this.list.filter((e)=>e.isFinish==true).length;
+			},
+			//计算属性的完整版的写法
+			newAll:{
+				get(){
+					return this.list.filter(e=>e.isFinish==true).length ===this.list.length;
+				},
+				set(val){
+						return	this.list.filter(e=>e.isFinish=val);
+				}
+			}
+		},
+		//侦听器完整版写法
+		watch:{
+			list:{	
+				handler(newArr){
+
+					window.localStorage.setItem('list',JSON.stringify(newArr))
+				},
+				deep:true
+			},
+			
+		}
+	})
+
+})(window, Vue);
